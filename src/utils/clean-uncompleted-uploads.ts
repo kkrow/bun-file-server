e2467e8abc2785dd3477c2aea19ba2ed63f21650
kkrow <kuro.kuro.kyuuketsuki@gmail.com>
2025-03-29 18:13:17 +0900

    fix path errors

--- src/utils/clean-uncompleted-uploads.ts
+++ src/utils/clean-uncompleted-uploads.ts
@@ -1,7 +1,6 @@
 import { $, env } from "bun";
-import { cwd } from "node:process";
 
 export const cleanUncompletedUploads = async () => {
-  const uploadsDir = cwd() + (env.ROOT_DIR || "/uploads");
+  const uploadsDir = env.ROOT_DIR || "/uploads";
   await $`find ${uploadsDir}/* -name "temp_*" -mtime +1 -delete`;
 };
